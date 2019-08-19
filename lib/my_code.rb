7035a238a5427b74660058e80a5966d37f88d1a0
Steve Alex <steve.alex@hotmail.co.uk>
2019-08-19 12:11:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,20 @@
-# Your Code Here
+def map(source_array)
+  array = []
+  source_array.each do |item|
+    array.push(yield(item))
+  end  
+  
+  array
+end
+
+
+def reduce(source_array, starting_point=0)
+  total = starting_point
+  
+  source_array.each do |item|
+    total += yield(item)
+  end  
+  
+  total
+end
+
